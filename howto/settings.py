ca947a1a4e17aaac8e3c9cdb9cbba87dd4b9ee10
frknnlzts <trpc61@gmail.com>
2017-12-29 03:18:44 +0300

    Last Commit

--- howto/settings.py
+++ howto/settings.py
@@ -192,7 +192,7 @@ AUTHENTICATION_BACKENDS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.howto'),
     }
 }
 
